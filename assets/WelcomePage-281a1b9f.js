a436d62a00d739ddb49bbf8e4abb7d844ff306cf
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-01-27 09:05:27 +0000

    Deploying to gh-pages from @ Tosha177777/5th-project-group@34afa64b8f0feb99cd04a75f6d682104d21be93e 🚀

--- assets/WelcomePage-281a1b9f.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-5b162c70.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("div",{children:"WelcomePage"})});export{t as default};
---
+++ assets/WelcomePage-7c7903e2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-b0217765.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("div",{children:"WelcomePage"})});export{t as default};

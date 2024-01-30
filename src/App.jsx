02e0aa07cc3a7c35bf8d444d30a8a6155f8641be
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-01-30 10:19:01 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 import { Header } from './components/Header/Header';
+import { Loader } from './components/Loader/Loader';
+import RestrictedRoute from './components/RestrictedRoutes/RestrictedRoute';
+import { AppContainer } from './App.styled';
 
 const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
 const SignUpPage = lazy(() => import('./pages/SignUpPage/SignUpPage'));
@@ -10,11 +13,19 @@ const appRoutes = [
   { path: '/', element: <WelcomePage /> },
   {
     path: '/sign-up',
-    element: <SignUpPage />,
+    element: (
+      <RestrictedRoute>
+        <SignUpPage />
+      </RestrictedRoute>
+    ),
   },
   {
-    path: '/sign-in',
-    element: <SigninPage />,
+    path: '/signin',
+    element: (
+      <RestrictedRoute>
+        <SigninPage />
+      </RestrictedRoute>
+    ),
   },
 ];
 
@@ -22,13 +33,15 @@ const App = () => {
   return (
     <>
       <Header />
-      <Suspense fallback={<div>Loader...</div>}>
-        <Routes>
-          {appRoutes.map(({ path, element }) => (
-            <Route key={path} path={path} element={element} />
-          ))}
-        </Routes>
-      </Suspense>
+      <AppContainer>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            {appRoutes.map(({ path, element }) => (
+              <Route key={path} path={path} element={element} />
+            ))}
+          </Routes>
+        </Suspense>
+      </AppContainer>
     </>
   );
 };

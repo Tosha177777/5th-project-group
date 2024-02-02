a27dde732e7a97e81dfd6d4ebf899dbcab57c8f4
Maksim <max.godnavi133@gmail.com>
2024-02-02 21:56:02 +0200

    StyleFix

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -2,6 +2,7 @@ import { Container } from './WelcomePage.styled';
 import WelcomeContainer from './WelcomeContainer/WelcomeContainer';
 import { WhyDrinkWater } from './WhyDrinkWater/WhyDrinkWater';
 import { WaterСonsumptionTracker } from './WaterСonsumptionTracker/WaterСonsumptionTracker';
+import { DailyNormaModal } from '../../components/DailyNormaModal/DailyNormaModal';
 
 const WelcomePage = () => {
   return (
@@ -10,6 +11,7 @@ const WelcomePage = () => {
         <WaterСonsumptionTracker />
         <WhyDrinkWater />
       </Container>
+      <DailyNormaModal></DailyNormaModal>
     </WelcomeContainer>
   );
 };

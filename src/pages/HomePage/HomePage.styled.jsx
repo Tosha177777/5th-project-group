8d249d66e82d8f5d267aecdc639ce07f3db5baee
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-02-07 20:17:53 +0200

    Update HomePage.styled.jsx

--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -10,18 +10,19 @@ export const HomeContainer = styled.div`
   left: 0;
   z-index: -10;
   background-image: url('src/images/mobile/bottle-home-screen@1x.png'),
-   url('src/images/mobile/background-element-home-screen@1x.png');
+    url('src/images/mobile/background-element-home-screen@1x.png');
   background-size: 280px, cover;
-  background-position: top 56px left 20px, top 0 left 0;
+  background-position:
+    top 56px left 20px,
+    top 0 left 0;
   background-repeat: no-repeat;
-  
-   
+
   @media (min-device-pixel-ratio: 2),
     (-webkit-min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
     background-image: url('src/images/mobile/bottle-home-screen@2x.png'),
-    url('src/images/mobile/background-element-home-screen@2x.png');
+      url('src/images/mobile/background-element-home-screen@2x.png');
   }
   @media (min-width: 768px) {
     max-width: 768px;
@@ -30,16 +31,18 @@ export const HomeContainer = styled.div`
     top: -16px;
     left: 0;
     background-image: url('src/images/tablet/bottle-home-screen@1x.png'),
-     url('src/images/tablet/background-element-home-screen@1x.png');
+      url('src/images/tablet/background-element-home-screen@1x.png');
     background-size: 518px, cover;
-    background-position: top 16px left 125px, top 0 left 0;
+    background-position:
+      top 16px left 125px,
+      top 0 left 0;
     @media (min-device-pixel-ratio: 2),
       (-webkit-min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
       background-image: url('src/images/tablet/bottle-home-screen@2x.png'),
-       url('src/images/tablet/background-element-home-screen@2x.png');
-    
+        url('src/images/tablet/background-element-home-screen@2x.png');
+    }
   }
   @media (min-width: 1440px) {
     max-width: 1404px;
@@ -48,17 +51,17 @@ export const HomeContainer = styled.div`
     top: 0;
     left: 9px;
     background-image: url('src/images/desktop/bottle-home-screen@1x.png'),
-    url('src/images/desktop/background-element-main-page@1x.png');
+      url('src/images/desktop/background-element-main-page@1x.png');
     background-size: 738px, 1404px;
-    background-position: top 0 left 30px, top 38px left 0;
-  
-     
-    @media (min-device-pixel-ratio: 2),
-           (min-resolution: 192dpi) {
-      background-image: url('src/images/desktop/bottle-home-screen@2x.png'), 
-      url('src/images/desktop/background-element-main-page@2x.png');
+    background-position:
+      top 0 left 30px,
+      top 38px left 0;
+
+    @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url('src/images/desktop/bottle-home-screen@2x.png'),
+        url('src/images/desktop/background-element-main-page@2x.png');
+    }
   }
-} 
 `;
 
 export const StyledContainer = styled.div`
@@ -66,7 +69,7 @@ export const StyledContainer = styled.div`
 
   @media (min-width: 1440px) {
     display: flex;
-    gap: 32px;   
+    gap: 32px;
   }
 `;
 
@@ -75,15 +78,14 @@ export const StyledInfoWrap = styled.div`
   background-color: ${colors.secondaryColor2WhiteBlue};
   border-radius: 10px;
   box-shadow: 0 4px 14px 0 rgba(64, 123, 255, 0.3);
-  
-  @media (min-width: 768px) {
-  padding: 32px 24px ;
 
-  @media (min-width: 1440px) {
-  width: 592px;
+  @media (min-width: 768px) {
+    padding: 32px 24px;
+
+    @media (min-width: 1440px) {
+      width: 592px;
+    }
   }
-  }
-  
 `;
 export const StyledTodayInfo = styled.div`
   height: 260px;
@@ -102,11 +104,10 @@ export const StyledTodayInfo = styled.div`
 `;
 
 export const StyledDataWrap = styled.div`
-margin-bottom: 40px;
+  margin-bottom: 40px;
 
-@media (min-width: 1440px) {
+  @media (min-width: 1440px) {
     padding-top: 32px;
     margin-bottom: 0;
   }
-`
-
+`;

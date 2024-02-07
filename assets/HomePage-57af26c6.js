52a31e1bad4251afaacc543330c9ecf57fba1db2
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-02-07 18:18:49 +0000

    Deploying to gh-pages from @ Tosha177777/5th-project-group@8d249d66e82d8f5d267aecdc639ce07f3db5baee 🚀

--- assets/HomePage-57af26c6.js
+++ assets/HomePage-57af26c6.js
@@ -1,4 +1,4 @@
-import{n as O,c as A,m as mt,p as Ao,f as rt,r as m,q as ko,j as f,t as kt,u as P,v as ji,b as Mi,w as Di,d as Ei,x as jo,y as Ri,z as Mo,A as Li,B as Do,R as Bi,C as Eo,D as Ro}from"./index-e15b25fc.js";const Lo=O.div`
+import{n as O,c as A,m as mt,p as Ao,f as rt,r as m,q as ko,j as f,t as kt,u as P,v as ji,b as Mi,w as Di,d as Ei,x as jo,y as Ri,z as Mo,A as Li,B as Do,R as Bi,C as Eo,D as Ro}from"./index-6278be61.js";const Lo=O.div`
 padding: 8px 20px;
 margin-bottom: 232px;
 width: 166px;
@@ -839,18 +839,19 @@ color: ${A.primaryColorWhite};
   left: 0;
   z-index: -10;
   background-image: url('src/images/mobile/bottle-home-screen@1x.png'),
-   url('src/images/mobile/background-element-home-screen@1x.png');
+    url('src/images/mobile/background-element-home-screen@1x.png');
   background-size: 280px, cover;
-  background-position: top 56px left 20px, top 0 left 0;
+  background-position:
+    top 56px left 20px,
+    top 0 left 0;
   background-repeat: no-repeat;
-  
-   
+
   @media (min-device-pixel-ratio: 2),
     (-webkit-min-device-pixel-ratio: 2),
     (min-resolution: 192dpi),
     (min-resolution: 2dppx) {
     background-image: url('src/images/mobile/bottle-home-screen@2x.png'),
-    url('src/images/mobile/background-element-home-screen@2x.png');
+      url('src/images/mobile/background-element-home-screen@2x.png');
   }
   @media (min-width: 768px) {
     max-width: 768px;
@@ -859,16 +860,18 @@ color: ${A.primaryColorWhite};
     top: -16px;
     left: 0;
     background-image: url('src/images/tablet/bottle-home-screen@1x.png'),
-     url('src/images/tablet/background-element-home-screen@1x.png');
+      url('src/images/tablet/background-element-home-screen@1x.png');
     background-size: 518px, cover;
-    background-position: top 16px left 125px, top 0 left 0;
+    background-position:
+      top 16px left 125px,
+      top 0 left 0;
     @media (min-device-pixel-ratio: 2),
       (-webkit-min-device-pixel-ratio: 2),
       (min-resolution: 192dpi),
       (min-resolution: 2dppx) {
       background-image: url('src/images/tablet/bottle-home-screen@2x.png'),
-       url('src/images/tablet/background-element-home-screen@2x.png');
-    
+        url('src/images/tablet/background-element-home-screen@2x.png');
+    }
   }
   @media (min-width: 1440px) {
     max-width: 1404px;
@@ -877,38 +880,37 @@ color: ${A.primaryColorWhite};
     top: 0;
     left: 9px;
     background-image: url('src/images/desktop/bottle-home-screen@1x.png'),
-    url('src/images/desktop/background-element-main-page@1x.png');
+      url('src/images/desktop/background-element-main-page@1x.png');
     background-size: 738px, 1404px;
-    background-position: top 0 left 30px, top 38px left 0;
-  
-     
-    @media (min-device-pixel-ratio: 2),
-           (min-resolution: 192dpi) {
-      background-image: url('src/images/desktop/bottle-home-screen@2x.png'), 
-      url('src/images/desktop/background-element-main-page@2x.png');
+    background-position:
+      top 0 left 30px,
+      top 38px left 0;
+
+    @media (min-device-pixel-ratio: 2), (min-resolution: 192dpi) {
+      background-image: url('src/images/desktop/bottle-home-screen@2x.png'),
+        url('src/images/desktop/background-element-main-page@2x.png');
+    }
   }
-} 
 `,sd=O.div`
   margin: 0 auto;
 
   @media (min-width: 1440px) {
     display: flex;
-    gap: 32px;   
+    gap: 32px;
   }
 `,od=O.div`
   padding: 24px 8px;
   background-color: ${A.secondaryColor2WhiteBlue};
   border-radius: 10px;
   box-shadow: 0 4px 14px 0 rgba(64, 123, 255, 0.3);
-  
-  @media (min-width: 768px) {
-  padding: 32px 24px ;
 
-  @media (min-width: 1440px) {
-  width: 592px;
+  @media (min-width: 768px) {
+    padding: 32px 24px;
+
+    @media (min-width: 1440px) {
+      width: 592px;
+    }
   }
-  }
-  
 `,rd=O.div`
   height: 260px;
   overflow-y: auto;
@@ -924,9 +926,9 @@ color: ${A.primaryColorWhite};
     border-radius: 8px;
   }
 `,ad=O.div`
-margin-bottom: 40px;
+  margin-bottom: 40px;
 
-@media (min-width: 1440px) {
+  @media (min-width: 1440px) {
     padding-top: 32px;
     margin-bottom: 0;
   }

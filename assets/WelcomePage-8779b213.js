11115e19f9a759e1e19d4848d889eae5bbd6d024
Tosha177777 <73199654+Tosha177777@users.noreply.github.com>
2024-02-07 18:47:24 +0000

    Deploying to gh-pages from @ Tosha177777/5th-project-group@fe799954df06bfad4fa9230112302fc29194f0db 🚀

--- assets/WelcomePage-8779b213.js
+++ assets/WelcomePage-8779b213.js
@@ -1,4 +1,4 @@
-import{u as e,j as A,c as n,r as o,a as s}from"./index-6278be61.js";const d=e.div`
+import{u as e,j as A,c as n,r as o,a as s}from"./index-683dc826.js";const d=e.div`
   @media screen and (max-width: 767px) {
     padding-bottom: 40px;
   }
